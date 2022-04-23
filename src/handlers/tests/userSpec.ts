96452941d109350a2f476e70e6a2f2969cc0aa19
Sameerjr1 <73047153+Sameerjr1@users.noreply.github.com>
2022-04-23 22:28:57 +0200

    user route tests done

--- src/handlers/tests/userSpec.ts
+++ src/handlers/tests/userSpec.ts
@@ -24,7 +24,7 @@ describe('api/users Endpoint testing', () => {
     await conn.query('ALTER SEQUENCE users_id_seq RESTART WITH 1');
     conn.release();
   });
-  describe('Testing authenticate route', async () => {
+  describe('[POST] /api/users/authenticate', async () => {
     it('Authenticate with correct parameters', async () => {
       const response = await request
         .post('/api/users/authenticate')
@@ -35,7 +35,72 @@ describe('api/users Endpoint testing', () => {
         })
         .expect(200);
       token = response.body;
-      console.log(token);
+    });
+    it('Authenticate with incorrect credentials', async () => {
+      await request
+        .post('/api/users/authenticate')
+        .set('Content-Type', 'application/json')
+        .send({
+          email: 'user2@example.com',
+          password: 'password13',
+        })
+        .expect(401);
+    });
+  });
+  describe('Testing CRUD Endpoints', () => {
+    it('[GET] /api/users with token', async () => {
+      const response = await request
+        .get('/api/users')
+        .set('Content-Type', 'application/json')
+        .set('Authorization', `Bearer ${token}`)
+        .expect(200);
+      expect(response.body.users.length).toEqual(1);
+    });
+    it('[GET] /api/users without token', async () => {
+      await request
+        .get('/api/users')
+        .set('content-type', 'application/json')
+        .expect(401);
+    });
+    it('[POST] /api/users with correct email', async () => {
+      const response = await request
+        .post('/api/users')
+        .send({
+          email: 'foo@bar.com',
+          first_name: 'John',
+          last_name: 'Smith',
+          password: 'password123',
+        })
+        .expect(200);
+      expect(response.body.status).toEqual(200);
+    });
+    it('[POST] /api/users with already existing email', async () => {
+      const response = await request.post('/api/users').send({
+        email: 'user2@example.com',
+        first_name: 'Abdelrahman',
+        last_name: 'Ahmed',
+        password: 'password123',
+      });
+      expect(response.status).toBe(422);
+    });
+    it('[GET] /api/users/:id with token', async () => {
+      const response = await request
+        .get('/api/users/1')
+        .set('Content-Type', 'application/json')
+        .set('Authorization', `Bearer ${token}`);
+      expect(response.status).toBe(200);
+      expect(response.body.data).toEqual({
+        id: user.id,
+        email: user.email,
+        first_name: user.first_name,
+        last_name: user.last_name,
+      });
+    });
+    it('[GET] /api/users/:id without token', async () => {
+      const response = await request
+        .get('/api/users/1')
+        .set('Content-Type', 'application/json');
+      expect(response.status).toBe(401);
     });
   });
 });
